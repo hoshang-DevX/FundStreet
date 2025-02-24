e149dc726c4b958c5bf9ff04da584ec2cd8ff998
Hoshang <hoshangdogra@gmail.com>
2025-02-24 17:16:07 +0530

    added Header

--- FundStreet Project/src/App.jsx
+++ FundStreet Project/src/App.jsx
@@ -1,9 +1,20 @@
+import { BrowserRouter as Router, Routes , Route } from "react-router-dom"
+import Home from './pages/Home'
+import About from './pages/About'
+import Header from "./components/header/Header"
 
 function App() {
   return (
-    <div>
-      FundStreet Project
-    </div>
+   
+   <Router>
+   <Header/>
+      <Routes>
+         <Route path="/" element = {<Home/>}  />  
+         <Route path="/about" element = {<About/>} />
+      </Routes>
+   </Router>
+
+   
   )
 }
 
